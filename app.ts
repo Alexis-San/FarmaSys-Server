3cbce533758277000cb1f2cd3d51b1bbd837ff2d
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-11-09 16:53:18 -0300

    fix(src) se agrego una carpeta src

--- app.ts
+++ app.ts
@@ -1,13 +1,6 @@
 import dotenv from 'dotenv';
-import Server from './models/server';
+import Server from './src/models/server';
 
 
-
- async function main() {
-    //configurar dot.env
 dotenv.config();
 const server = new Server();
-
-server.listen();
- }
-
--- src/routes/cliente.ts
+++ src/routes/cliente.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import {getClientes, getCliente, postCliente,putCliente,deleteCliente} from '../controllers/clientes';
+import {getClientes, getCliente, postCliente, deleteCliente} from '../controllers/clientes';
 
 const router=Router();
 router.get('/',getClientes);
 router.get('/:id',getCliente);
 router.post('/',postCliente);
-router.put('/:id',putCliente);
+//router.put('/:id',putCliente);
 router.delete('/:id',deleteCliente);
 
 
