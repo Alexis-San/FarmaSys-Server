f0759a20205cde9e53903c189eee01299aa4de0f
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-11-19 17:54:08 -0300

    refactor(categoria): mejorar formato y estructura del modelo Categoria

--- src/models/categoria.ts
+++ src/models/categoria.ts
@@ -1,21 +1,25 @@
-import { DataTypes } from 'sequelize';
-import db from '../db/connection';
+import { DataTypes } from "sequelize";
+import db from "../db/connection";
 
-const categoria = db.define('categoria',{
-
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    nombre:{
-        type:DataTypes.STRING,
-        allowNull: false,
-    },
-    descripcion:{
-        type:DataTypes.TEXT,  
-    },
-})
+const categoria = db.define("categoria", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  descripcion: {
+    type: DataTypes.TEXT,
+  },
+  //este campo es para eliminacion logica
+  estado: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+});
 
 export default categoria;
