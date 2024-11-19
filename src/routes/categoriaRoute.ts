058ba6d0a457458d0729ea9cdc270af0bc9439c3
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-11-19 17:54:17 -0300

    feat(categoria): agregar rutas para gestionar operaciones CRUD de categorías

---
+++ src/routes/categoriaRoute.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  getCategorias,
+  getCategoria,
+  postCategoria,
+  putCategoria,
+  deleteCategoria,
+} from "../controllers/categoriaController";
+
+const router = Router();
+
+router.get("/", getCategorias);
+router.get("/:id", getCategoria);
+router.post("/", postCategoria);
+router.put("/:id", putCategoria);
+router.delete("/:id", deleteCategoria);
+
+export default router;
