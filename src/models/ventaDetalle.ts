ea63786fc797b32b2df549289b62bd27d9840bdd
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-12-02 20:23:57 -0300

    feat(ventaDetalle): refactor model to use class-based structure with TypeScript interfaces

--- src/models/ventaDetalle.ts
+++ src/models/ventaDetalle.ts
@@ -1,35 +1,68 @@
-import { DataTypes } from "sequelize";
+import { DataTypes, Model } from "sequelize";
 import db from "../db/connection";
 import Venta from "./ventas";
 import Inventario from "./inventario";
 
-const VentaDetalle = db.define("ventaDetalle", {
-  id_venta: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: Venta,
-      key: "id_venta",
+interface VentaDetalleAttributes {
+  id_venta: number;
+  id_producto_inventario: number;
+  precio: number;
+  cantidad: number;
+  monto_total: number;
+  estado?: boolean;
+}
+
+class VentaDetalle
+  extends Model<VentaDetalleAttributes>
+  implements VentaDetalleAttributes
+{
+  public id_venta!: number;
+  public id_producto_inventario!: number;
+  public precio!: number;
+  public cantidad!: number;
+  public monto_total!: number;
+  public estado!: boolean;
+}
+
+VentaDetalle.init(
+  {
+    id_venta: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Venta,
+        key: "id_venta",
+      },
+    },
+    id_producto_inventario: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Inventario,
+        key: "id",
+      },
+    },
+    precio: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    cantidad: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    monto_total: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      defaultValue: 0, //precio*cantidad
+    },
+    estado: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
     },
   },
-  id_producto_inventario: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: Inventario,
-      key: "id",
-    },
-  },
-  precio: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-  },
-  cantidad: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  monto_total: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-  },
-});
+  {
+    sequelize: db,
+    modelName: "ventaDetalle",
+    tableName: "ventaDetalle",
+  }
+);
 
 export default VentaDetalle;
