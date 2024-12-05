66dc22cf85ec766880b4a8fd914260d8a81d7968
Alexis Javier Sanabria Gaona <Alejavierpy@gmail.com>
2024-12-04 21:33:50 -0300

    feat(informes): update ventas search route to accept query parameter

--- src/routes/informesRoutes.ts
+++ src/routes/informesRoutes.ts
@@ -9,7 +9,7 @@ import {
 } from "../controllers/informesController";
 
 const router = Router();
-router.get("/ventas/buscar/", obtenerVentasFiltradas);
+router.get("/ventas/buscar/:busqueda", obtenerVentasFiltradas);
 router.get("/ventas", obtenerVentasInforme);
 router.get("/ventas/ultimo-mes", obtenerVentasUltimoMes);
 router.get("/ventas/cliente/:clienteId", obtenerHistorialVentasCliente);
